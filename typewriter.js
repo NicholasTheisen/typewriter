a0985257b8051e17b0a5d15b3f438727eea89742
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-11 03:19:57 +0000

    Typewriter.js

---
+++ typewriter.js
@@ -0,0 +1,12 @@
+const sentence = "hello there from lighthouse labs";
+
+for (let i = 0; i < sentence.length; i++) {
+  setTimeout(() => {
+    process.stdout.write(sentence[i]);
+    
+    // Check if it is the last character
+    if (i === sentence.length - 1) {
+      process.stdout.write('\n');
+    }
+  }, 50 * i);
+}
